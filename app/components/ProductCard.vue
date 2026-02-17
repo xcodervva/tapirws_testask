c1ed483390804878e9a2b90a344db1f8c85b38ba
xcoder <onlyforwith@gmail.com>
2026-02-17 10:14:34 +0300

    Add ProductCard component

---
+++ app/components/ProductCard.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { Product } from '~/types/Product';
+
+const props = defineProps<{
+  product: Product;
+}>();
+
+const halfPrice = computed<number>(() => {
+  return Math.ceil(props.product.price / 2);
+});
+</script>
+
+<template>
+  <a
+    href="#"
+    class="product-card"
+  >
+    <div class="product-card__image-wrapper">
+      <img
+        :src="product.image"
+        :alt="product.name"
+        class="product-card__image"
+      />
+
+      <button
+        type="button"
+        class="product-card__favorite"
+      >
+        Добавить в избранное
+      </button>
+    </div>
+
+    <div class="product-card__content">
+      <h3 class="product-card__title">
+        {{ product.name }}
+      </h3>
+
+      <div class="product-card__prices">
+        <span class="product-card__price">
+          {{ product.price }} ₽
+        </span>
+
+        <span class="product-card__price product-card__price--half">
+          x2: {{ halfPrice }} ₽
+        </span>
+      </div>
+    </div>
+  </a>
+</template>
