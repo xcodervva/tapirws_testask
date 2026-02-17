a86929fc02391dd1a2fc9097a405e6f1a94c143f
xcoder <onlyforwith@gmail.com>
2026-02-17 10:38:47 +0300

    Add AppHeader component

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <div class="header__container">
+      <a href="#" class="header__logo">Логотип</a>
+
+      <nav class="header__nav">
+        <a href="#" class="header__link">Главная</a>
+        <a href="#" class="header__link">Каталог</a>
+        <a href="#" class="header__link">Контакты</a>
+      </nav>
+
+      <button class="header__burger">☰</button>
+    </div>
+  </header>
+</template>
