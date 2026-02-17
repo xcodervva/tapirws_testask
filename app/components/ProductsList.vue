4dd8bdc2b7b4aa59b2507a9d2bb852e6ba114c08
xcoder <onlyforwith@gmail.com>
2026-02-17 10:12:12 +0300

    Update ProductList component

---
+++ app/components/ProductsList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Product } from '~/types/Product';
+import ProductCard from '~/components/ProductCard.vue';
+
+defineProps<{
+  products: Product[];
+}>();
+</script>
+
+<template>
+  <div class="products-list">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+</template>
